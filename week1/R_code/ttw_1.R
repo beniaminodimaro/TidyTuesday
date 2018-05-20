a5bbea4180781f0bf93aaf295c01f36a785b3a7b
beniaminodimaro <beniamino.dimaro@gmail.com>
2018-05-20 09:39:26 +0000

    Change directory name

---
+++ week1/R_code/ttw_1.R
@@ -0,0 +1,12 @@
+# Install all necessary libraries
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("here")
+
+# Loading the libraries
+library(tidyverse)
+library(readxl)
+library(here)
+
+# Loading raw dataset
+here("data", "R_code")
