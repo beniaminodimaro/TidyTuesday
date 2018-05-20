6c385b931dc7e2f34d2bfd3b5fb116b113ded9b4
beniaminodimaro <beniamino.dimaro@gmail.com>
2018-05-20 13:01:52 +0000

    extract average data and create variable to plot

--- week1/R_code/ttw_1.R
+++ week1/R_code/ttw_1.R
@@ -2,11 +2,15 @@
 install.packages("tidyverse")
 install.packages("readxl")
 install.packages("here")
+install.packages("glue")
+install.packages("ggrepel")
 
 # Loading the libraries
 library(tidyverse)
 library(readxl)
 library(here)
+library(glue)
+library(ggrepel)
 
 # Loading raw dataset
 data <- dir(here("week1", "data"), full.names = TRUE, pattern = "us_avg") %>%
@@ -15,5 +19,24 @@ data <- dir(here("week1", "data"), full.names = TRUE, pattern = "us_avg") %>%
 # tidying data
 
 tidy_data <- data %>%
-  gather(year, avg_tuition , -State)
+  gather(year, avg_tuition , -State) %>%
+  rename(state = State)
+
+# calculate the average national tuition for the years 2004-05 and 2015-16
+
+nat_avg <- tidy_data %>%
+  filter(year %in% c("2004-05", "2015-16")) %>%
+  group_by(year) %>%
+  summarize(avg_tuition = mean(avg_tuition)) %>%
+  mutate(state = "National Average")
+
+# extracting the years 2004-2005 and 2015-2016 from tidy_data and adding
+# the National Average
+
+plot_data <- tidy_data %>%
+  filter(year %in% c("2004-05", "2015-16")) %>%
+  group_by(year) %>%
+  left_join(select(nat_avg, year, nat_avg = avg_tuition), by = "year") %>%
+  bind_rows(nat_avg)
+
 
